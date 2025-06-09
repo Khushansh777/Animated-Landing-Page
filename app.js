bad38e3135b2f4893be36e1d9a4b8e6118f57f7b
Khushansh <khushanshbaliyan@gmail.com>
2025-06-09 14:40:01 +0530

    Add animations and styling for landing page elements; implement GSAP ScrollTrigger for enhanced interactivity

---
+++ app.js
@@ -0,0 +1,95 @@
+// Register ScrollTrigger plugin
+gsap.registerPlugin(ScrollTrigger);
+
+function page1Aninmation() {
+const tl = gsap.timeline()
+tl.from('nav .logo',{
+    y:30,
+    opacity:0,
+    stagger:0.1,
+    delay:0.15,
+})
+tl.from('.nav-links, nav button',{
+     y:30,
+    opacity:0,
+    stagger:.1,
+})
+
+tl.from('#hero #part1',{
+    x:-200,
+    opacity:0,
+    // delay:0.2,
+    // stagger:0.3,
+},'-=0.6')
+
+tl.from('#hero #part2',{
+    opacity:0,
+},'-=0.5')
+
+gsap.from('.logos-test img',{
+    y:-30,
+    opacity:0,
+    stagger:0.1,
+    scrollTrigger:{
+        markers:true,
+        scroller:'body',
+        trigger:'.logos-test',
+        start:'top 90%',
+        end:'bottom 100%'
+    }
+})
+}
+// page1Aninmation()
+const container = document.querySelectorAll('.elem');
+container.forEach(element => {
+       element.addEventListener('mouseover',() =>{
+   gsap.to(element,{
+    scale:1.1,
+}) 
+})
+
+element.addEventListener('mouseleave',() =>{
+    gsap.to(element,{
+        scale:1,
+    })
+})
+});
+
+const tl2 = gsap.timeline({
+scrollTrigger:{
+            markers:true,
+            scroller:'body',
+            trigger:'.container',
+            start:'top 80%',
+            scrub:2,
+            end:'top 20%'
+        }
+})
+const leftConstainer = document.querySelectorAll('.elem.left');
+const rightContainer = document.querySelectorAll('.elem.right');
+
+tl2.from('.elem.line-1.left',{
+    x:-300,
+    opacity:0,
+    delay:0.5,
+},'lmao-1')
+tl2.from('.elem.line-2.left',{
+    x:300,
+    opacity:0,
+    delay:0.5,
+},'lmao-1')
+tl2.from('.elem.line-1.right',{
+    x:-300,
+    opacity:0,
+    delay:0.5,
+},'lmao-2','-=0.5')
+tl2.from('.elem.line-2.right',{
+    x:300,
+    opacity:0,
+    delay:0.5,
+},'lmao-2','-=0.5')
+
+
+
+ 
+
